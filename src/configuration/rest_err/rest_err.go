c34685a8c45f3b7612c7cfc27670607777ba9fcc
felipepereira2697 <felipepereira2697@gmail.com>
2023-12-06 23:28:53 +0000

    100 Days of Code - Creating new methods to handle different http errors

--- src/configuration/rest_err/rest_err.go
+++ src/configuration/rest_err/rest_err.go
@@ -1,13 +1,51 @@
 package resterr
 
+import "net/http"
+
 type RestErr struct {
 	Message string   `json:"message"`
 	Err     string   `json:"error"`
 	Code    int      `json:"code"`
-	Causes  []Causes `json:"causes"`
+	Causes  []Causes `json:"causes,omitempty"`
 }
 
 type Causes struct {
 	Field   string `json:"field"`
 	Message string `json:"message"`
 }
+
+// Defining a constructor
+func NewRestErr(message string, err string, code int, causes []Causes) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     err,
+		Code:    code,
+		Causes:  causes,
+	}
+}
+
+func NewBadRequestError(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     "bad_request",
+		Code:    http.StatusBadRequest,
+	}
+}
+func NewBadRequestValidationError(message string, causes []Causes) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     "bad_request",
+		Code:    http.StatusBadRequest,
+		Causes:  causes,
+	}
+}
+
+func NewInternalServerError(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     "internal_server_error",
+		Code:    http.StatusInternalServerError,
+	}
+}
+
+//@TODO: Create errors for: NotFound and 403 for authentication
